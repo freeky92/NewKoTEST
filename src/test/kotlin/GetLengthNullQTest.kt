4c5f697e4f1347fc17c8ad968ff5553f57860c56
freeky92 <1arichev@mail.ru>
2021-10-25 14:37:03 +0300

    21.10.2021

--- src/test/kotlin/GetLengthNullQTest.kt
+++ src/test/kotlin/GetLengthNullQTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class GetLengthNullQTest : MyFunSpec() {
 
     init {
-        test("GetLength1Test 1") {}
+        test("GetLength 1 Test 1") {}
         val result = getLength1(
             "MT3C0V5IA7EN2VLEAJYU3PWHJ6X4OGGXIHBHHKLYQZH" +
                     "5738HNVZRTGO0MHGRZGZDKDS9SIQ7S3Q5L9L2QISAXDRW1W7F156SYM"
@@ -16,7 +16,7 @@ class GetLengthNullQTest : MyFunSpec() {
     }
 
     init {
-        test("GetLength2Test 1") {}
+        test("GetLength 2 Test 1") {}
         val result = getLength2(
             "MT3C0V5IA7EN2VLEAJYU3PWHJ6X4OGGXIHBHHKLYQZH" +
                     "5738HNVZRTGO0MHISAXDRW1W7F156SYM"
@@ -26,19 +26,19 @@ class GetLengthNullQTest : MyFunSpec() {
     }
 
     init {
-        test("GetLength2Test 2") {}
+        test("GetLength 2 Test 2") {}
         val result = getLength2(null)
         result shouldBe 0
         println(result)
     }
 
     init {
-        test("GetLength3Test 1") {}
+        test("GetLength 3 Test 1") {}
         shouldThrow<NullPointerException> { getLength3(null) }
     }
 
     init {
-        test("GetLength3Test 2") {}
+        test("GetLength 3 Test 2") {}
         val result = getLength3(
             "Q9FQ0H58DECMM1OMFZ7NJZQ6QEH6HQ7282Q4YI28F9Z64DE" +
                     "PJRCU5YQKVW6GINLUFTHRO6ZM5F5MY6BLCPOWUF8OA"
