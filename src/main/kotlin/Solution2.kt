665727e1e18ee907aa60a0d843326755bf5505aa
freeky92 <1arichev@mail.ru>
2021-10-25 09:32:26 +0300

    21.10.2021

--- src/main/kotlin/Solution2.kt
+++ src/main/kotlin/Solution2.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Solution2
 
 fun calculateEvenDigits(input: String): Int {
@@ -6,7 +8,7 @@ fun calculateEvenDigits(input: String): Int {
 
     for (i in input) {
         val even: Int = i.digitToInt()
-        if ((even % 2) == 0){
+        if ((even % 2) == 0) {
             evenSum += even
         }
     }
@@ -14,15 +16,38 @@ fun calculateEvenDigits(input: String): Int {
 }
 
 
-fun calculateBugMentions(input:List<String>): Int{
+fun calculateBugMentions(input: List<String>): Int {
     var bugsCount = 0
     val bug = "BUG"
-    for (i in input){
-        if (i.uppercase() == bug){
+    for (i in input) {
+        if (i.uppercase() == bug) {
             bugsCount++
         }
     }
     return bugsCount
+}
+
+fun calculateWordStat(input: String): String {
+
+    val words: List<String> = input.split(" ")
+
+    // println(words.groupingBy { it }.eachCount().filter { it.value > 1 })
+
+    var maxC = 0
+    var maxCWord = ""
+
+    /*words.distinct() - отбрасываем повторения, для сокращения работы алгоритма.
+    * Collections.frequency(list, елемент типа T) - подсчитывает количество указанных элементов
+    * */
+
+    for (item in words.distinct()) {
+        if (maxC < Collections.frequency(words, item)) {
+            maxC = Collections.frequency(words, item)
+            maxCWord = item
+        }
+    }
+    println(maxC)
+    return maxCWord
 }
 
 
--- src/test/kotlin/CalculateEvenDigitTest.kt
+++ src/test/kotlin/CalculateEvenDigitTest.kt
@@ -1,16 +1,26 @@
 import custom_classes.MyFunSpec
 import io.kotest.matchers.shouldBe
 
-class CalculateEvenDigitTest : MyFunSpec(){
+class CalculateEvenDigitTest : MyFunSpec() {
 
     init {
-        test("Test 1 59408235"){
+        test("Test 1 59408235") {
             calculateEvenDigits("59408235") shouldBe 14
         }
     }
+
     init {
-        test("Test 2 660308340"){
+        test("Test 2 660308340") {
             calculateEvenDigits("660308340") shouldBe 24
+        }
+    }
+
+    init {
+        test("calcWordTest 1") {
+            val c = calculateWordStat("PDDPEM MXWHL XJID XJID XJID DTQEHGC DTQEHGC MXWHL XJID DTQEHGC DTQEHGC MXWHL PDDPEM QUCO DTQEHGC" +
+                    " MXWHL DTQEHGC MXWHL PDDPEM XJID MXWHL DTQEHGC DTQEHGC XJID XJID XJID MXWHL MXWHL MXWHL MXWHL DTQEHGC PDDPEM DTQEHGC" +
+                    " DTQEHGC PDDPEM")
+            println(c)
         }
     }
 
