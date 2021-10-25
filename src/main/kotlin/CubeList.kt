06609351529a66c12b2019c4afd57ea6f0b337b8
freeky92 <1arichev@mail.ru>
2021-10-25 11:56:32 +0300

    21.10.2021

--- src/main/kotlin/CubeList.kt
+++ src/main/kotlin/CubeList.kt
@@ -5,6 +5,10 @@ fun getCubeList(n: Int): List<Int> {
     return List(n) { i -> i * i * i }
 }
 
-fun getLength(str: String?): Int? {
+fun getLength1(str: String?): Int? {
     return str?.length
+}
+
+fun getLength2(str: String?): Int {
+    return str?.length?:0
 }
--- src/test/kotlin/GetLengthNullQTest.kt
+++ src/test/kotlin/GetLengthNullQTest.kt
@@ -4,12 +4,30 @@ import io.kotest.matchers.shouldBe
 class GetLengthNullQTest : MyFunSpec() {
 
     init {
-        test("GetLengthTest 1") {}
-        val result = getLength(
+        test("GetLength1Test 1") {}
+        val result = getLength1(
             "MT3C0V5IA7EN2VLEAJYU3PWHJ6X4OGGXIHBHHKLYQZH" +
                     "5738HNVZRTGO0MHGRZGZDKDS9SIQ7S3Q5L9L2QISAXDRW1W7F156SYM"
         )
         result shouldBe 98
+        println(result)
+    }
+
+    init {
+        test("GetLength2Test 1") {}
+        val result = getLength2(
+            "MT3C0V5IA7EN2VLEAJYU3PWHJ6X4OGGXIHBHHKLYQZH" +
+                    "5738HNVZRTGO0MHISAXDRW1W7F156SYM"
+        )
+        result shouldBe 98
+        println(result)
+    }
+
+    init {
+        test("GetLength2Test 2") {}
+        val result = getLength2(null)
+        result shouldBe 0
+        println(result)
     }
 
 }
